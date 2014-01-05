fc960782a02338a44d2db6c419bd870b49b236e3
Tushar Pokle <tushar.pokle@gmail.com>
2014-01-05 06:21:24 +0000

    Fixed up IP addresses so that you can connect to cassandra!

--- src/start.sh
+++ src/start.sh
@@ -3,9 +3,16 @@
 IP=`hostname --ip-address`
 echo Configuring Cassandra to listen at $IP 
 
-CONFIG=/etc/cassandra/conf/cassandra.yaml
-sed -i -e "s/^listen_address.*/listen_address: $IP/"   $CONFIG
-#sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"   $CONFIG
+DEFAULT=/etc/cassandra/default.conf
+CONFIG=/etc/cassandra/conf
 
-echo Starting Cassandra...
+rm -rf $CONFIG
+mkdir -p $CONFIG
+cp $DEFAULT/cassandra.yaml $DEFAULT/log4j-server.properties $CONFIG
+
+sed -i -e "s/^listen_address.*/listen_address: $IP/"   $CONFIG/cassandra.yaml
+sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"   $CONFIG/cassandra.yaml
+sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$IP\"/" $CONFIG/cassandra.yaml
+
+echo Starting Cassandra... 
 cassandra -f
