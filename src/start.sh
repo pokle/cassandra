15934b3d594d65bf883e152e317a8715e1f0212a
flaviof <fferreira@ubiwhere.com>
2014-01-27 18:12:47 +0000

    fixing nodetool

--- src/start.sh
+++ src/start.sh
@@ -10,11 +10,12 @@ CONFIG=/etc/cassandra/conf
 
 rm -rf $CONFIG
 mkdir -p $CONFIG
-cp $DEFAULT/cassandra.yaml $DEFAULT/log4j-server.properties $CONFIG
+cp $DEFAULT/cassandra.yaml $DEFAULT/log4j-server.properties $DEFAULT/cassandra-env.sh $CONFIG
 
 sed -i -e "s/^listen_address.*/listen_address: $IP/"   $CONFIG/cassandra.yaml
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"   $CONFIG/cassandra.yaml
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CONFIG/cassandra.yaml
+sed -i -e "s/# JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=<public name>\"/ JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=$IP\"/" $CONFIG/cassandra-env.sh
 
 echo Starting Cassandra... 
 cassandra -f
